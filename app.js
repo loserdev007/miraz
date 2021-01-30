573351cec7dcb73fa1f527ae5b165b5bc07201ec
looserdev7 <mdnahid3339@gmail.com>
2021-01-30 16:17:00 +0000

    Style added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const listEl = document.querySelector('.main');
-const sectionsEl = document.querySelectorAll('.sec');
-listEl.addEventListener('click', listHandler);
-const nperEl = document.querySelector('.main > li').clientHeight;
-document.querySelector('.nav span').style.height = `${nperEl}px`;
+const navElList = document.querySelector('.main');
+const sectionsListEl = document.querySelectorAll('.sec');
+navElList.addEventListener('click', navHandler);
 let prev = 1;
-const allSec = document.querySelectorAll('.sec');
-console.log(allSec[0].getBoundingClientRect().top);
-function listHandler(event) {
+let isClicked = false;
+let isScrolling;
+
+function navHandler(event) {
+   isClicked = true;
+   const perNav = document.querySelector('.main > li').clientHeight;
    const checkId = event.target.closest('.checkPoint').id;
    const perEl = document.querySelectorAll('.sec')[0].clientHeight;
    const span = document.querySelector('.nav span');
@@ -16,31 +17,33 @@ function listHandler(event) {
          behavior: 'smooth',
          block: 'center'
       });
-      span.style.transform = `translateY(${nperEl * (checkId - 1)}px)`;
-      console.log(nperEl * (checkId - 1));
+      span.style.transform = `translateY(${perNav * (checkId - 1)}px)`;
       prev = checkId;
    }
 }
 window.onscroll = function () {
+   window.clearTimeout(isScrolling);
+   isScrolling = setTimeout(function () {
+      isClicked = false;
+   }, 100);
+   if (isClicked) return;
+   const perNav = document.querySelector('.main > li').clientHeight;
    const span = document.querySelector('.nav span');
-   if (allSec[0].getBoundingClientRect().top <= 0 && allSec[0].getBoundingClientRect().bottom >= 0) {
-      span.style.transform = `translateY(${nperEl * 0}px)`;
+   if (sectionsListEl[0].getBoundingClientRect().top <= 0 && sectionsListEl[0].getBoundingClientRect().bottom >= 0) {
+      span.style.transform = `translateY(${perNav * 0}px)`;
       prev = 1;
-   }
-   else if (allSec[1].getBoundingClientRect().top <= 0 && allSec[1].getBoundingClientRect().bottom >= 0) {
-      span.style.transform = `translateY(${nperEl * 1}px)`;
+   } else if (sectionsListEl[1].getBoundingClientRect().top <= 0 && sectionsListEl[1].getBoundingClientRect().bottom >= 0) {
+      span.style.transform = `translateY(${perNav * 1}px)`;
       prev = 2;
-   }
-   else if (allSec[2].getBoundingClientRect().top <= 0 && allSec[2].getBoundingClientRect().bottom >= 0) {
-      span.style.transform = `translateY(${nperEl * 2}px)`;
+   } else if (sectionsListEl[2].getBoundingClientRect().top <= 0 && sectionsListEl[2].getBoundingClientRect().bottom >= 0) {
+      span.style.transform = `translateY(${perNav * 2}px)`;
       prev = 3;
    }
-   else if (allSec[3].getBoundingClientRect().top <= 0 && allSec[3].getBoundingClientRect().bottom >= 0) {
-      span.style.transform = `translateY(${nperEl * 3}px)`;
-      prev = 4;
-   }
-   else if (allSec[4].getBoundingClientRect().top <= 0 && allSec[4].getBoundingClientRect().bottom >= 0) {
-      span.style.transform = `translateY(${nperEl * 4}px)`;
-      prev = 5;
-   }
+   // else if (sectionsListEl[3].getBoundingClientRect().top <= 0 && sectionsListEl[3].getBoundingClientRect().bottom >= 0) {
+   //    span.style.transform = `translateY(${perNav * 3}px)`;
+   //    prev = 4;
+   // } else if (sectionsListEl[4].getBoundingClientRect().top <= 0 && sectionsListEl[4].getBoundingClientRect().bottom >= 0) {
+   //    span.style.transform = `translateY(${perNav * 4}px)`;
+   //    prev = 5;
+   // }
 }
